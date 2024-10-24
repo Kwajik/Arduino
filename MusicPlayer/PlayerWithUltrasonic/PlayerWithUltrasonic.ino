814857ccc5afa3532702997631216397a2bcdb5c
HongDeveloper <60872762+Kwajik@users.noreply.github.com>
2024-10-25 02:18:08 +0900

    change method order

--- MusicPlayer/PlayerWithUltrasonic/PlayerWithUltrasonic.ino
+++ MusicPlayer/PlayerWithUltrasonic/PlayerWithUltrasonic.ino
@@ -8,8 +8,8 @@ SoftwareSerial DFPlayerSerial(10, 11); // RX, TX
 DFRobotDFPlayerMini DFPlayer;
 const unsigned long CHECK_INTERVAL = 2000;
 const float STOP_STATUS = 0;
-const int VOLUME = 15; // Set volume value. From 0 to 30
-const int TRACK = 1;
+const int VOLUME = 14; // Set volume value. From 0 to 30
+const int TRACK = 6;
 
 // Ultrasonic 
 const byte SONIC_TRIGGER = 5;
@@ -42,21 +42,8 @@ void InitializePlayer()
             delay(0); // Code to compatible with ESP8266 watch dog.
         }
     }
+    DFPlayer.EQ(DFPLAYER_EQ_BASS);
     Serial.println(F("DFPlayer Mini online."));
-}
-
-void Play()
-{
-    // Serial.println("Play");
-
-    DFPlayer.volume(VOLUME); 
-    DFPlayer.play(TRACK); 
-
-    while (true) {
-        if (!IsPlaying()) break;
-        
-        delay(CHECK_INTERVAL); 
-    }
 }
 
 void loop()
@@ -75,6 +62,20 @@ bool IsDetected()
 
     // Serial.println(distance); 
     return distance <= DETECT_DISTANCE ;
+}
+
+void Play()
+{
+    // Serial.println("Play");
+
+    DFPlayer.volume(VOLUME); 
+    DFPlayer.play(TRACK); 
+
+    while (true) {
+        if (!IsPlaying()) break;
+
+        delay(CHECK_INTERVAL); 
+    }
 }
 
 bool IsPlaying()
